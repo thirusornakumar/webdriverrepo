6e45f020e4927ce43f45c97b0e578281300deaa9
thirusornakumar <thirusornakumar1987@gmail.com>
2020-07-16 01:13:38 +0530

    Initial commit

---
+++ src/test/java/webtesting/testngmethodTest.java
@@ -0,0 +1,28 @@
+package webtesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testngmethodTest {
+
+	
+	@Test
+	public  void method1() throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("hello2");;
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://rediff.com");
+		
+		Thread.sleep(4000);
+		
+		driver.close();
+		driver.quit();
+		
+	}
+
+}
---
+++ src/test/java/webtesting/webtesting.java
@@ -0,0 +1,25 @@
+package webtesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class webtesting {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("hello2");;
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://rediff.com");
+		
+		Thread.sleep(4000);
+		
+		driver.close();
+		driver.quit();
+		
+	}
+
+}
